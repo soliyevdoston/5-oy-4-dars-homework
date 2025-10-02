7f6ec5170972b3b89a61a3698f6cc11fc39ab3a4
dostonbek <dostonbek>
2025-10-02 09:55:06 +0500

    2

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
 const elCerdTemp = document.getElementById("cardTemp");
 const elParent = document.getElementById("parent");
 
-
-
-
 fetch("https://json-api.uz/api/project/fn43/cars")
-.then((res)=>{
-    return res.json();
-})
-.then((res)=>{
+  .then((res) => res.json())
+  .then((res) => {
     ui(res.data);
-    })
-    .catch(()=>{})
-    .finally(()=>{});
+  })
+  .catch(() => {})
+  .finally(() => {});
 
-    function ui(cars){
-        cars.forEach(car => {
-            const elCard = elCerdTemp.content.cloneNode(true);
-            elCard.querySelector(".card-title").textContent = car.name;
-            elCard.querySelector("p").textContent = car.description;
-            elParent.appendChild(elCard);
-        })};
+function ui(cars) {
+  cars.forEach((car) => {
+    const elCard = elCerdTemp.content.cloneNode(true);
+
+    // Title
+    elCard.querySelector(".card-title").textContent = car.name;
+
+    // Description with innerHTML (original English labels)
+    elCard.querySelector(".card").classList.add("card-stil");
+
+    elCard.querySelector("p").innerHTML = `
+      <b>Trim:</b> ${car.trim} <br>
+      <b>Generation:</b> ${car.generation} <br>
+      <b>Year:</b> ${car.year} <br>
+      <b>Color:</b> ${car.color} <br>
+      <b>Category:</b> ${car.category} <br>
+      <b>Door Count:</b> ${car.doorCount}, <b>Seat Count:</b> ${car.seatCount} <br>
+      <b>Max Speed:</b> ${car.maxSpeed} <br>
+      <b>Acceleration:</b> ${car.acceleration} <br>
+      <b>Engine:</b> ${car.engine}, ${car.horsepower} HP <br>
+      <b>Fuel Type:</b> ${car.fuelType} <br>
+      <b>Fuel Consumption:</b> City – ${car.fuelConsumption.city}, Highway – ${car.fuelConsumption.highway}, Combined – ${car.fuelConsumption.combined} <br>
+      <b>Country:</b> ${car.country} <br>
+      <b>Description:</b> ${car.description}
+    `;
+
+    elParent.appendChild(elCard);
+  });
+}
